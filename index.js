744554c71a65b5be5f0b74b32fad384e184e2506
KITTIPONG WONGSADITYANON <6431503005@lamduan.mfu.ac.th>
2024-09-22 14:35:20 +0700

    ลองแก้

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ async function initializeDBConnection() {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
+            database: process.env.DB_NAME,
+            port: process.env.PORT,
         });
         console.log("Connected To MySQL");
     } catch (err) {
